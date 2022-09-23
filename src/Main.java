925315aa42053e8683914845526a680df7ec40b9
Gabriel Kloper <gabrielkloper@gmail.com>
2022-09-23 14:23:20 -0300

    fixed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Área dos Quadrilateros");
+        Quadrilatero.area(3);
+        Quadrilatero.area(2d,8d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(2f,8f);
+    }
+}
